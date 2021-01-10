bccaa741369fdd4526d1de8f6f13dda209af7eb5
Paweł <pawel.sikora1999@interia.pl>
2021-01-10 15:40:49 +0100

    Added routing php

--- index.php
+++ index.php
@@ -1,3 +1,13 @@
 <?php
 
-echo 'Hello there 👋';
+require 'Routing.php';
+
+$path = trim($_SERVER['REQUEST_URI'], '/');
+$path = parse_url($path, PHP_URL_PATH);
+
+Routing::get('index', 'DefaultController');
+Routing::get('home', 'DefaultController');
+Routing::get('news', 'DefaultController');
+Routing::get('my library', 'DefaultController');
+Routing::get('rankings', 'DefaultController');
+Routing::run($path);
