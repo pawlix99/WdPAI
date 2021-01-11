9bd97eb7b59c734d2d7b2f2b13212c7dd401910b
Paweł <pawel.sikora1999@interia.pl>
2021-01-11 03:31:47 +0100

    Added connection with database

---
+++ config.php
@@ -0,0 +1,6 @@
+<?php
+
+const USERNAME = 'jauhduydtszkkj';
+const PASSWORD = 'ac500f58e4b49ed79d3a3cb6f7f75396eda930550f165980a708788d415771f7';
+const HOST = 'ec2-54-247-71-245.eu-west-1.compute.amazonaws.com';
+const DATABASE = 'dd5oenkt64sino';
